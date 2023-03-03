7c185bfd6f4387c7bd706dbe53bd937d2ed6409d
SergiiMal <ser4ester@gmail.com>
2023-03-03 19:07:18 +0200

    read_first

---
+++ src/js/readRevisionDate/readRevisionDate.js
@@ -0,0 +1,22 @@
+	// let readRevision = document.querySelector('.revision-date-arrow');
+	// let content  = document.querySelector('.gallery-text');
+
+	// readRevision.addEventListener('click', ()=>{
+	// 	if(readRevision.classList.contains('up')){
+	// 		readRevision.classList.add('down');			
+	// 		readRevision.classList.remove('up');
+	// 	} else{
+	// 		readRevision.classList.add('up');
+	// 		readRevision.classList.remove('down')
+	// 		}
+	// 	if(content.style.maxHeight){
+	// 		content.style.maxHeight = null;
+	// 	}else{
+	// 		content.style.maxHeight = null;
+	// 		content.style.maxHeight = 
+	// 		content.scrollHeight + 'px'
+	// 	}		
+	// })
+
+
+
