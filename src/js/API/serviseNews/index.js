b8d115df2b6b6ebf630cd4cb736259b5dc31ee1a
Roman-Nechaev <nrchaevroman@gmail.com>
2023-03-04 16:20:58 +0200

    use

--- src/js/API/serviseNews/index.js
+++ src/js/API/serviseNews/index.js
@@ -1,3 +1,5 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 import NewsApiServis from './serviseNewsSearch';
 
 import { templateMarkupNews } from './templateMarkupNews';
@@ -11,22 +13,34 @@ const newsApiServis = new NewsApiServis();
 
 export default function onSearchForm(e) {
   e.preventDefault();
-  let { value } = e.target.search;
+  let { value } = e.target.searchQuery;
+
   value = value.trim();
+  if (!value) {
+    Notify.warning('The search field should not be empty!');
+    return;
+  }
   clearGalleryInterface();
   newsApiServis.query = value;
   requestToServer();
 }
 
 async function requestToServer() {
+  let arr = [];
   try {
     const data = await newsApiServis.fetchNewsApi();
     const newsDateResponse = await data.response.docs;
-    console.log(newsDateResponse);
+    if (newsDateResponse.length <= 0) {
+      Notify.info(
+        'Sorry, there are no news matching your search query. Please try again.'
+      );
+    }
 
     renderTemplate(newsDateResponse);
 
-    const useID = newsDateResponse.map(onId => testFavorit(onId._id));
+    const useID = newsDateResponse.map(onId => arr.push(onId._id));
+
+    testFavorit(arr);
   } catch (error) {}
 }
 
@@ -38,15 +52,76 @@ function clearGalleryInterface() {
   galleryRef.innerHTML = '';
 }
 
-function testFavorit(params) {
-  const clickOneCards = document.querySelector('[data-id]');
-  console.log(clickOneCards.dataset.id);
+Notify.init({
+  width: '300px',
 
-  const favouriteBtnRef = document.querySelector('.add-to-favBtn');
-  favouriteBtnRef.addEventListener('click', evt => {
-    const reservedId = params; // зарезервований ID
+  position: 'center-top',
+  distance: '80px',
 
-    // console.log(`Вибраний ID: ${selectedId}`);
-    console.log(reservedId);
-  });
+  warning: {
+    background: 'rgba(68,64,247,0.7)',
+    textColor: '#FFFFFF',
+    childClassName: 'notiflix-notify-warning',
+    notiflixIconColor: '#FFFFFF',
+    fontAwesomeClassName: 'fas fa-exclamation-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(238,191,49,0.2)',
+  },
+  info: {
+    background: 'rgba(68,64,247,0.7)',
+    textColor: '#FFFFFF',
+    childClassName: 'notiflix-notify-info',
+    notiflixIconColor: '#FFFFFF',
+    fontAwesomeClassName: 'fas fa-info-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(38,192,211,0.2)',
+  },
+});
+
+const cardRef = document.querySelector('.gallery');
+
+cardRef.addEventListener('click', onClickCard);
+
+function onClickCard(evt) {
+  if (evt.target.nodeName === 'BUTTON') {
+    // console.log('УРАА КНОПКА!!');
+
+    const clickOneCards = document.querySelectorAll('.newsHomePage-card');
+
+    for (const card of clickOneCards) {
+      card.addEventListener('click', e => {
+        const id = e.currentTarget;
+
+        const testing = id.outerHTML;
+
+        localStorage.setItem('savedNews', JSON.stringify(testing));
+      });
+    }
+    return;
+  } else {
+    // console.log('Click!!!!!!!!!!!');
+    return;
+  }
 }
+
+// ========================================
+// function testFavorit(arrId) {
+//   // console.log(arrId);
+
+//   const reservedId = 'nyt://article/606a4ef1-13b8-5136-bc59-e8a5463ec075'; // зарезервований ID
+
+//   const arr = arrId; // масив ID
+//   let selectedId = null;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === reservedId) {
+//       console.log(arr[i]);
+//       selectedId = arr[i];
+//       break;
+//     }
+//   }
+
+//   console.log(`Вибраний ID: ${selectedId}`);
+// }
+
+// let foo = JSON.parse(localStorage.getItem('savedNews'));
