0209d842d58ff2f61c6b3ff9ad05e4bcce0bb03a
Anton Tyndyk <tyndykanton1178@gmail.com>
2023-03-04 03:22:17 +0200

    feature/footer added

---
+++ src/js/formSubscription/formSubscription.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('footer-form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const emailInput = document.getElementById('footer-emailut');
+  const email = emailInput.value;
+  console.log(email);
+  form.reset();
+});
