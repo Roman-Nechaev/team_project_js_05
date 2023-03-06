9f76469d4747faf880937b1048328341b653d87f
Nazar Voitekh <nazar.voitekh@gmail.com>
2023-03-06 15:28:15 +0200

    first_calendar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import './js/API/serviseNews/index';
-// import './js/addFavourite/addFavourite';
+import './js/addFavourite/addFavourite';
 
-// import './js/addFavourite/renderFavourite';
+import './js/addFavourite/renderFavourite';
 
 import './js/header/mobileSearch';
 import './js/header/headerTheme';
-// import './js/calendar/calendar';
+import './js/calendar/calendar';
 import './js/readRevisionDate/readRevisionDate';
 
-// import './js/allLogicSearch/index';
+import './js/allLogicSearch/index';
 
-// import './js/API/serviseNews/index';
-import './js/allLogicSearch/markupNews/renderNews'
-
+import './js/API/serviseNews/index';
+import './js/allLogicSearch/markupNews/renderNews';
