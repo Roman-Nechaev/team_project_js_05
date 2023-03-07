8d7f6ad0370be6569258a3722f522a24fe6d487a
Roman-Nechaev <nrchaevroman@gmail.com>
2023-03-07 17:37:14 +0200

    add default page fav read

--- src/js/addFavourite/addFavourite.js
+++ src/js/addFavourite/addFavourite.js
@@ -12,7 +12,7 @@ let incomingСardsSearch;
 export default function addFavourite(cardsSearch) {
   incomingСardsSearch = cardsSearch;
 }
-
+// chectPage(incomingСardsHome, incomingСardsSearch);
 let arrayOfCardsSelectedById =
   JSON.parse(localStorage.getItem('FavouriteStorage')) || [];
 
@@ -30,7 +30,7 @@ function onClikGalleryHome(e) {
 
   if (incomingСardsHome) {
     incomingСardsHome.forEach(news => {
-      testLocal(arrayOfCardsSelectedById, cardsHomeId);
+      console.log(testLocal(arrayOfCardsSelectedById, cardsHomeId));
       if (news.id == cardsHomeId) {
         arrayOfCardsSelectedById.push(news);
         localStorage.setItem(
@@ -71,10 +71,15 @@ function onClikGalleryHome(e) {
 
 function testLocal(arr, idCardsClick) {
   // console.log('arr :>> ', arr);
-  // console.log(idCardsClick);
+
   let allIdCards;
-  arr.map(ons => {
+  let contr;
+  return arr.filter(ons => {
     allIdCards = ons.id || ons._id;
+    const foo = allIdCards == idCardsClick;
+    console.log(foo);
+    if (foo) {
+      return;
+    }
   });
-  // console.log(idCardsClick == allIdCards);
 }
---
+++ src/js/defaultPage/defaultPageHome.js
@@ -0,0 +1,11 @@
+const defaultPageRef = document.querySelector('.default-favourite');
+const favConPageRef = document.querySelector('.gallery-favourite');
+
+export function chectPage(arr1, arr2) {
+  console.log(arr1);
+  console.log(arr2);
+  //   console.log(local);
+  //   local.length === 0
+  //     ? favConPageRef.classList.add('dis-none')
+  //     : defaultPageRef.classList.add('dis-none');
+}
