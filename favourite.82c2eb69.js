76aaba0a19133aa802c3d2fc9aea278bf36b111a
Roman-Nechaev <89315040+Roman-Nechaev@users.noreply.github.com>
2023-03-07 16:45:34 +0000

    Deploying to gh-pages from @ Roman-Nechaev/team_project_js_05@e523354593d40118ae17d7d463e4f1caaa45f4aa 🚀

---
+++ favourite.82c2eb69.js
@@ -0,0 +1,2 @@
+!function(){var e,o=document.querySelector(".default-favourite"),t=document.querySelector(".gallery-favourite"),a=JSON.parse(localStorage.getItem("FavouriteStorage"))||[];e=a,console.log(e),0===e.length?t.classList.add("dis-none"):o.classList.add("dis-none")}();
+//# sourceMappingURL=favourite.82c2eb69.js.map
---
+++ read.3de9023a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".default-read"),t=document.querySelector(".revision-date");let d=JSON.parse(localStorage.getItem("readMore"))||[];0===d.length?t.classList.add("dis-none"):e.classList.add("dis-none");
+//# sourceMappingURL=read.3de9023a.js.map
