94a2468798581396cd3fbc2a43a905f88854e424
Evgeniy-Nazarov <perfectyam@gmail.com>
2023-03-09 23:31:10 -0500

    Update burger.js

--- src/js/burger/burger.js
+++ src/js/burger/burger.js
@@ -1,19 +1,19 @@
 
-const burgerBtn = document.querySelector('.burger-btn');
-const btnClose = document.querySelector('.closeburgmenu');
+const burgerBtn = document.getElementById('burger-open');
+const btnClose = document.getElementById('burger-close');
 
 burgerBtn.addEventListener('click', openMenu);
 
 
 function openMenu() {
-  const menuOpen = document.querySelector('.burger-menu');
+  const menuOpen = document.getElementById('burger-menu_add');
     
   menuOpen.classList.toggle('is-open');
 }
 
 btnClose.addEventListener('click', closeMenu);
 function closeMenu() {
-  const menuOpen = document.querySelector('.burger-menu');
+  const menuOpen = document.getElementById('burger-menu_add');
     
   if (menuOpen.classList.contains('is-open')) {
     menuOpen.classList.remove('is-open');
