7c9a77c2d41d1ae818c0ff31bf66bcbce1584678
Roman-Nechaev <nrchaevroman@gmail.com>
2023-03-10 14:31:23 +0200

    fix path  headerCurrent

--- src/js/header/headerCurrent.js
+++ src/js/header/headerCurrent.js
@@ -1,19 +1,17 @@
 const href = window.location.href;
 const path = href.split('/');
-
+console.log(path);
 function pageCurrent() {
-    if (path[3] === 'index.html') {
-        document.getElementById('home').classList.add('current');
-        document.getElementById('mobile-home').classList.add('current');
-    } else if (path[3] === 'favourite.html') {
-        document.getElementById('favorite').classList.add('current');
-        document.getElementById('mobile-favorite').classList.add('current');
-    } else if (path[3] === 'read.html') {
-        document.getElementById('read').classList.add('current');
-        document.getElementById('mobile-read').classList.add('current');
-    }
+  if (path[4] === 'index.html') {
+    document.getElementById('home').classList.add('current');
+    document.getElementById('mobile-home').classList.add('current');
+  } else if (path[4] === 'favourite.html') {
+    document.getElementById('favorite').classList.add('current');
+    document.getElementById('mobile-favorite').classList.add('current');
+  } else if (path[4] === 'read.html') {
+    document.getElementById('read').classList.add('current');
+    document.getElementById('mobile-read').classList.add('current');
+  }
 }
 
 pageCurrent();
-
-
